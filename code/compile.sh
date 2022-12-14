d992492b94476d8d5e1d54f18a8f3b662b0f23d9
ale <aledeum.saf@gmail.com>
2022-12-14 14:47:56 -0300

    Made it so that the compile script renames the output from code to code.out, so that is able to be included in the .gitignore. Added code/code.out to .gitignore

--- code/compile.sh
+++ code/compile.sh
@@ -2,4 +2,10 @@
 
 cd build
 make
-cp ../code "$EREBUS_DIRECTORY/game/controllers/robot0Controller/robot0Controller"
+
+FILE=../code
+if test -f "$FILE"; then
+    mv ../code ../code.out
+fi
+
+cp ../code.out "$EREBUS_DIRECTORY/game/controllers/robot0Controller/robot0Controller"
