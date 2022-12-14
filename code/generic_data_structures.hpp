304a884c6701554d4909d6e53619ef5fdc7f10f7
ale <aledeum.saf@gmail.com>
2022-12-14 14:33:04 -0300

    Added Vector3D data structures to store values in the commonly used x y z format

---
+++ code/generic_data_structures.hpp
@@ -0,0 +1,13 @@
+class FloatVector3D{
+    public:
+        float x;
+        float y;
+        float z;
+    };
+
+class DoubleVector3D{
+    public:
+        float x;
+        float y;
+        float z;
+    };
