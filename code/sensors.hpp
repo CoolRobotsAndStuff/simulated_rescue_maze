15e92515e3053e568a34981da1045fe3cfd02c1c
ale <aledeum.saf@gmail.com>
2022-12-15 11:54:46 -0300

    added sensor manager class

--- code/sensors.hpp
+++ code/sensors.hpp
@@ -1,6 +1,8 @@
 #include <webots/GPS.hpp>
 #include <webots/Gyro.hpp>
+#include <webots/Robot.hpp>
 #include "generic_data_structures.hpp"
+
 
 namespace sensors{
     class GPS{
@@ -14,8 +16,13 @@ namespace sensors{
             DoubleVector3D velocity;
             DoubleVector3D previous_velocity;
             DoubleVector3D acceleration;
-
+            
+            // default constructor
+            GPS();
+            // normal constructor
             GPS(webots::GPS *gps_device, int time_step);
+
+            void init(webots::GPS *gps_device, int time_step);
 
             void update();
             void print_values();
@@ -34,12 +41,26 @@ namespace sensors{
             DoubleVector3D angular_velocity;
             DoubleVector3D angular_acceleration_rads;
             DoubleVector3D angular_acceleration;
-        
+
+            // default constructor
+            Gyroscope();
+            // normal constructor
             Gyroscope(webots::Gyro *gyro_device, int time_step);
+
+            void init(webots::Gyro *gyro_device, int time_step);
 
             void update();
             void print_values(bool convert_to_degs);
     };
 
+
+    class SensorManager{
+
+        public:
+            GPS gps;
+            Gyroscope gyroscope;
+            SensorManager(webots::Robot *robot, int time_step);
+            void update();
+    };
     
 }
