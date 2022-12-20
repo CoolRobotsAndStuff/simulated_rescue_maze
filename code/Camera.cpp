713c8008ea2e7002995590381606e51aeab31a56
JoaquinRodriguez2006 <joaquienrodriguez760@gmail.com>
2022-12-20 20:39:46 -0300

    add changes improving camera and updating .sh code

---
+++ code/Camera.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/Camera.hpp>
+#include <webots/Emitter.hpp>
+#include <webots/GPS.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
+
+using namespace webots;
+using namespace cv;
+using namespace std;
+
