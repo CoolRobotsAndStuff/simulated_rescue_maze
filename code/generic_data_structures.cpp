bbdfe06638f3cd5e221aa34c27c6d76fb4d2543e
ale <aledeum.saf@gmail.com>
2023-01-04 11:36:11 -0300

    added differential velocities data class

--- code/generic_data_structures.cpp
+++ code/generic_data_structures.cpp
@@ -3,6 +3,26 @@
 
 using namespace std;
 
+DifferentialVelocities::DifferentialVelocities(){}
+
+DifferentialVelocities::DifferentialVelocities(float left, float right){
+    set_velocities(left, right);
+}
+
+std::array<float, 2> DifferentialVelocities::get_array(){
+    return std::array<float, 2>{left, right};
+};
+
+void DifferentialVelocities::set_array(std::array<float, 2> input_array){
+    left = input_array[0];
+    right = input_array[1];
+};
+
+void DifferentialVelocities::set_velocities(float left, float right){
+    this->left = left;
+    this->right = right;
+}
+
 void FloatVector3D::print(){
     cout << "x: " << x << " y: " << y << " z: " << z << endl;
 }
--- code/generic_data_structures.hpp
+++ code/generic_data_structures.hpp
@@ -1,4 +1,17 @@
 #pragma once
+#include <array>
+
+class DifferentialVelocities{
+    public:
+        float left;
+        float right;
+    
+        DifferentialVelocities();
+        DifferentialVelocities(float left, float right);
+        std::array<float, 2> get_array();
+        void set_array(std::array<float, 2> input_array);
+        void set_velocities(float left, float right);
+};
 
 class FloatVector3D{
     public:
