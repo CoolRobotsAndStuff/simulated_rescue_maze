c4e8ab4ea0df72f57bc3b69865660aa1e5da1384
ale <aledeum.saf@gmail.com>
2023-01-04 11:37:10 -0300

    added change_range functions for floats and doubles

--- code/utils.cpp
+++ code/utils.cpp
@@ -23,3 +23,27 @@ double normalize_rads(double rads){
         rads += 2 * M_PI;
     return rads;
 }
+
+float change_float_range(float value, float min, float max, float new_min, float new_max){
+    float old_range = (max - min);
+    if (old_range == 0){
+        return min;
+    }    
+    else
+    {
+        float new_range = (new_max - new_min);
+        return (((value - min) * new_range) / old_range) + new_min;
+    }
+}
+
+double change_double_range(double value, double min, double max, double new_min, double new_max){
+    double old_range = (max - min);
+    if (old_range == 0){
+        return min;
+    }    
+    else
+    {
+        double new_range = (new_max - new_min);
+        return (((value - min) * new_range) / old_range) + new_min;
+}
+}
--- code/utils.hpp
+++ code/utils.hpp
@@ -2,3 +2,5 @@ double rads2degs(double rads);
 double degs2rads(double degs);
 double normalize_degs(double degs);
 double normalize_rads(double degs);
+float change_float_range(float value, float min, float max, float new_min, float new_max);
+double change_double_range(double value, double min, double max, double new_min, double new_max);
