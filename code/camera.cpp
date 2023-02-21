6cdf10e53da23084d17db27520ff04b8def38129
JoaquinRodriguez2006 <joaquienrodriguez760@gmail.com>
2023-02-21 18:27:44 -0300

    add improvements in camera

--- code/camera.cpp
+++ code/camera.cpp
@@ -1,39 +1,54 @@
 #include <iostream>
 #include <webots/Robot.hpp>
 #include <opencv2/opencv.hpp>
+#include <webots/Camera.hpp>
 
 using namespace webots;
 using namespace cv;
 using namespace std;
 
-// INITIALIZATIONS
-Robot *robot = new Robot();
-int timeStep = (int)robot->getBasicTimeStep();      // Set the time step for the simulation
+int main(int argc, char **argv) {
 
-// Camera Initialization
-Camera *cam = robot->getCamera("camera1");
-cam->enable(timeStep);
+  Class Camara {
 
-
-class camera {
-    public:
-        int image;
-    
+  public:
     void get_image() {
-          Mat opencv_image = cam.get_image();
-          cv::imshow("CameraView", opencv_image);
-      }  
-};
+      Mat frame(cam->getHeight(), cam->getWidth(), cv_8uc4, (void*)cam->getImage());
+      imshow("Frame", frame);
+    }
+    void RGB_scale() {
+      cvtColor(frame, frame, COLOR_BGR2GRAY);
+      imshow("greyScale", frame);
 
-int main() {
+    void threshold() {
+      threshold(frame, frame, 80, )
+    }
+    }
+  }
+  
+  Robot *robot = new Robot();
 
+  Camera *cam = robot->getCamera("camera1");
+
+  int timeStep = (int)robot->getBasicTimeStep();
+
+  cam->enable(timeStep);
+ 
   while (robot->step(timeStep) != -1) {
 
-  camera myCamera;
-
-  myCamera.get_image();
-  }
+    Mat frame(cam->getHeight(), cam->getWidth(), CV_8UC4, (void*)cam->getImage());
     
+    imshow("frame", frame);
+    
+    cvtColor(frame,frame,COLOR_BGR2GRAY); // grayscale
+    imshow("grayScale", frame);
+    
+    threshold(frame,frame,80,255,THRESH_BINARY); // threshold
+    imshow("thresh", frame);
+    
+    waitKey(1); // Render imshows on screen
+  };
+
   delete robot;
   return 0;
 }
